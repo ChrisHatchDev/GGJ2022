f2b492a989ff2b00c7bd81316f8092cb6eac740b
Chris Hatch <contact@chrishatch.dev>
2022-01-29 14:11:39 -0500

    WIP Add Syringe and Player Classes

---
+++ FleshBois/Assets/Code/FleshBoi.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FleshBoi : MonoBehaviour
+{
+    public ScoreKeeper scoreKeeper;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ FleshBois/Assets/Code/Syringe.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Juice
+{
+    Red = 0,
+    Blue = 1,
+    Green = 2,
+}
+
+public class Syringe : MonoBehaviour
+{
+    public Juice JuiceType;
+    public Transform NeedleEndPoint;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Activate()
+    {
+    }
+
+    public void EnteredTumor()
+    {
+
+    }
+
+    public void ExitedTumor()
+    {
+        
+    }
+}
