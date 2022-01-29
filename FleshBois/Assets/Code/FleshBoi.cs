d6dd7738a573901eaa40afa44916e83f3d1c2dbb
Chris Hatch <contact@chrishatch.dev>
2022-01-29 17:55:09 -0500

    Working Trigger Input with Registering on Each Hand Class

--- FleshBois/Assets/Code/FleshBoi.cs
+++ FleshBois/Assets/Code/FleshBoi.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class FleshBoi : MonoBehaviour
 {
     public ScoreKeeper scoreKeeper;
 
-    void Start()
+    public Syringe ActiveSyrings;
+    
+    public PlayerInput playerInput;
+
+    void Awake()
     {
-        
+        playerInput.actions.FindActionMap("XRI HMD").Enable();
+        playerInput.actions.FindActionMap("XRI LeftHand").Enable();
+        playerInput.actions.FindActionMap("XRI RightHand").Enable();
     }
 
     void Update()
@@ -31,5 +38,4 @@ public class FleshBoi : MonoBehaviour
 
         
     }
-
 }
--- FleshBois/Assets/FleshBoiHand.cs
+++ FleshBois/Assets/FleshBoiHand.cs
@@ -2,10 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public enum Hand
+{
+    Left = 0,
+    Right = 1,
+}
 
 public class FleshBoiHand : MonoBehaviour
 {
     public Syringe HeldSyringe;
+    public Text DebugTextObject;
+    public Text DebugTriggerText;
+
+    public Hand hand;
+
+    public bool PlungerIsActive;
 
     void Start()
     {
@@ -17,14 +31,61 @@ public class FleshBoiHand : MonoBehaviour
         if (ev.interactableObject.transform.tag == "Syringe")
         {
             HeldSyringe = ev.interactableObject.transform.GetComponent<Syringe>();
+            
+            if (DebugTextObject) DebugTextObject.text = HeldSyringe.gameObject.name;
         }
     }
 
     public void OnSelectExit(SelectExitEventArgs ev)
     {
-        if (ev.interactableObject.transform.gameObject == HeldSyringe)
+        if (ev.interactableObject.transform.gameObject == HeldSyringe.gameObject)
         {
             HeldSyringe = null;
+            if (DebugTextObject) DebugTextObject.text = "";
+        }
+    }
+
+    public void OnUIPress(InputValue val)
+    {
+        bool _triggerPressed = val.Get<bool>();
+
+        if (DebugTriggerText) DebugTriggerText.text = _triggerPressed.ToString();
+
+        if (_triggerPressed)
+        {
+
+        }
+    }
+
+    public void OnLeftTrigger(InputValue context)
+    {
+        if (hand != Hand.Left) return;
+
+        float _triggerPressed = context.Get<float>();
+
+        if (DebugTriggerText) DebugTriggerText.text = _triggerPressed.ToString();
+
+        if (_triggerPressed > 0.5f)
+        {
+            PlungerIsActive = true;
+        } else {
+            PlungerIsActive = false;
+        }
+    }
+
+    public void OnRightTrigger(InputValue context)
+    {
+        if (hand != Hand.Right) return;
+
+        float _triggerPressed = context.Get<float>();
+
+        if (DebugTriggerText) DebugTriggerText.text = _triggerPressed.ToString();
+
+        if (_triggerPressed > 0.5f)
+        {
+            PlungerIsActive = true;
+        } else {
+            PlungerIsActive = false;
         }
     }
 }
